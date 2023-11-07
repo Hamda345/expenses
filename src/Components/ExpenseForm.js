a00a457d9a62bacff82b8b8849824fd1dceb23b3
Hamda345 <hdallel13@gmail.com>
2023-11-07 16:42:58 +0100

    1st Commit

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,60 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
-import { useState } from "react";
-const ExpenseForm = ({getData}) => {
-    const  [userInput, setUserInput] = useState({
-        enteredTitle: "",
-        entredPrice: "",
-        enteredDate: "",
+
+const ExpenseForm = ({ getData }) => {
+  const [userInput, setUserInput] = useState({
+    entredTitle: "",
+    entredPrice: "",
+    entredDate: "",
+  });
+  const titleChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, entredTitle: event.target.value };
     });
-    const titleChangeHandler = (event) => {
-        setUserInput((prevState) => {
-            return {...prevState, enteredTitle: event.target.value};
-        });
-    };
-    const priceChangeHandler = (event) => {
-        setUserInput((prevState) => {
-            return{...prevState, entredPrice: event.target.value};
-        });
-    };
-      const dateChangeHandler = (event) => {
+  };
+  const priceChangeHandler = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, entredPrice: event.target.value };
+    });
+  };
+  const dateChangeHandler = (event) => {
     setUserInput((prevState) => {
       return { ...prevState, entredDate: event.target.value };
     });
   };
   const submitHandler = (event) => {
     event.preventDefault();
-    // getData(userInput);
     const exp = {
-            title: userInput.enteredTitle,
-            price: userInput.entredPrice,
-            date: new Date(userInput.enteredDate),
-            id:Math.floor().toString(),
-        };
-        getData(exp);
+      title: userInput.entredTitle,
+      price: userInput.entredPrice,
+      date: new Date(userInput.entredDate),
+      id: Math.random().toString(),
+    };
+    getData(exp);
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input type="text" value={userInput.entredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
-          <input type="number" step={0.001} onChange={priceChangeHandler}/>
+          <input type="number" value={userInput.entredPrice} step="0.001" onChange={priceChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date"
+          <input
+            type="date"
             min="2020-01-01"
             max="2024-12-31"
-            onChange={dateChangeHandler}/>
+            value={userInput.entredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
         <button>Cancel</button>
-        <button>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
